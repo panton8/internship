be9e4cc59e9dac6ef1ed933bb65d9a67786d8cad
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-21 21:39:17 +0300

    Add db migrations and admin creation

--- entrypoint-django.sh
+++ entrypoint-django.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-exec python manage.py runserver 0.0.0.0:8000 "$@"
+#!/bin/sh
+echo "Works well"
+python manage.py migrate
+echo "import os; from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(os.getenv('ADMIN_USERNAME'), os.getenv('ADMIN_EMAIL'), os.getenv('ADMIN_PASSWORD'), os.getenv('ADMIN_EMAIL'), os.getenv('ADMIN_PASSWORD'))" | python manage.py shell
+python manage.py runserver 0.0.0.0:8000
+$@
