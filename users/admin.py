beac36c71e0de1961c34e201e42c6122df3475eb
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-23 01:30:32 +0300

    Update user representation

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "role", "balance", "is_active")
