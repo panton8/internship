c09460eb3432a3932c7fbf0149eca7a05d926266
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-24 18:53:18 +0300

    Add logout url

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import LoginView, RegistrationView, UserViewSet
+from .views import LoginView, LogoutView, RegistrationView, UserViewSet
 
 router = DefaultRouter()
 router.register("users", UserViewSet)
@@ -10,4 +10,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("signup/", RegistrationView.as_view()),
     path("signin/", LoginView.as_view()),
+    path("logout/", LogoutView.as_view()),
 ]
