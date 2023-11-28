4e78e03037afb49d0aa960d2bd6ce259b4904761
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-28 15:30:40 +0300

    Update with absolute import and clear unnecessary paths

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (LoginView, LogoutView, RegistrationView, UserUpdateAPIView,
-                    UserViewSet)
+from users.views import UserViewSet
 
 router = DefaultRouter()
 router.register("users", UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("signup/", RegistrationView.as_view()),
-    path("signin/", LoginView.as_view()),
-    path("logout/", LogoutView.as_view()),
-    path("user/", UserUpdateAPIView.as_view()),
 ]
