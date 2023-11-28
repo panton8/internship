33f5f45fd9bc48e96d92cde0ed0c5b797ed0e1b7
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-28 17:08:34 +0300

    Update exception handling

--- users/middleware.py
+++ users/middleware.py
@@ -1,27 +1,3 @@
-# from rest_framework.response import Response
-# from rest_framework import status, exceptions
-#
-# from users.authentication import JWTAuthentication
-#
-#
-# class JWTMiddleware:
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-#
-#     def __call__(self, request):
-#         auth_class = JWTAuthentication()
-#
-#         try:
-#             user, token = auth_class.authenticate(request)
-#             request.user = user
-#             request.auth = token
-#         except exceptions.AuthenticationFailed as auth_error:
-#             request.error = str(auth_error.detail)
-#
-#         response = self.get_response(request)
-#
-#         return response
-
 from rest_framework import exceptions
 
 from users.authentication import JWTAuthentication
