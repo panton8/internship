f7f04e04b53807c7e7069c817f38454d5f715b51
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-29 23:58:20 +0300

    Add stocks app

--- crypto_project/settings.py
+++ crypto_project/settings.py
@@ -40,8 +40,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
+
     "users.apps.UsersConfig",
+    "stocks.apps.StocksConfig",
+
     "drf_yasg",
 ]
 
