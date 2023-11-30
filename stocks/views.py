b4884be919f42ca27ac59666648d7fa2750b977e
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-30 18:44:35 +0300

    Add views for each model

--- stocks/views.py
+++ stocks/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import mixins, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from stocks.models import Crypto, Order, Subscription, Wallet
+from stocks.serializers import (CryptoSerializer, OrderSerializer,
+                                SubscriptionSerializer, WalletSerializer)
+
+
+class WalletViewSet(
+    mixins.ListModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet
+):
+    queryset = Wallet.objects.all()
+    serializer_class = WalletSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class OrderViewSet(
+    mixins.ListModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet
+):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CryptoViewSet(
+    mixins.ListModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet
+):
+    queryset = Crypto.objects.all()
+    serializer_class = CryptoSerializer
+    permission_classes = [AllowAny]
+
+
+class SubscriptionViewSet(
+    mixins.ListModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet
+):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated]
