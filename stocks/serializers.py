a420299e24334fc067588130232dc9ad4ebf7992
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-30 18:42:24 +0300

    Add serializers for stocks app models

---
+++ stocks/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+from stocks.models import Crypto, Order, Subscription, Wallet
+from users.serializers import UserSerializer
+
+
+class CryptoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crypto
+        fields = ["name", "capacity", "exchange_rate"]
+
+
+class WalletSerializer(serializers.ModelSerializer):
+    user = UserSerializer().fields.get("username")
+    crypto = CryptoSerializer().fields.get("name")
+
+    class Meta:
+        model = Wallet
+        fields = ["user", "crypto", "amount"]
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    user = UserSerializer().fields.get("username")
+    crypto = CryptoSerializer().fields.get("name")
+
+    class Meta:
+        model = Subscription
+        fields = ["user", "crypto"]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = UserSerializer().fields.get("username")
+    crypto = CryptoSerializer().fields.get("name")
+
+    class Meta:
+        model = Order
+        fields = ["user", "crypto", "order_type", "total_price", "amount", "is_auto"]
