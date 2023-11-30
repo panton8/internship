988a7bf787f32fc67b6661bcf23bfb383378b4c8
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-30 15:49:24 +0300

    Remove unnecessary parameters

--- users/managers.py
+++ users/managers.py
@@ -13,8 +13,6 @@ class UserManager(BaseUserManager):
             email=self.normalize_email(email),
             password=password,
             username=username,
-            first_name=first_name,
-            last_name=last_name,
         )
         user.set_password(password)
         user.save()
