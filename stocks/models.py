f1bc7a1df3b782944172e5ad3c8f91021fc16910
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-30 12:20:39 +0300

    Add models

--- stocks/models.py
+++ stocks/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from users.models import User
@@ -5,7 +6,7 @@ from users.models import User
 
 class Crypto(models.Model):
     name = models.CharField()
-    capacity = models.FloatField
+    capacity = models.FloatField()
     exchange_rate = models.FloatField()
 
 
@@ -18,5 +19,25 @@ class Order(models.Model):
     crypto = models.ForeignKey(Crypto, on_delete=models.CASCADE)
     order_type = models.CharField(max_length=8, choices=Type.choices)
     total_price = models.FloatField(default=0.0)
-    amount = models.IntegerField()
-    is_auto = models.BooleanField
+    amount = models.FloatField(
+        validators=[
+            MinValueValidator(0.1),
+            MaxValueValidator(
+                limit_value=lambda value: Crypto.objects.get(
+                    pk=value.crypto_id
+                ).capacity
+            ),
+        ]
+    )
+    is_auto = models.BooleanField()
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    crypto = models.ForeignKey(Crypto, on_delete=models.CASCADE)
+
+
+class Wallet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    crypto = models.ForeignKey(Crypto, on_delete=models.CASCADE)
+    amount = models.FloatField(validators=[MinValueValidator(0.0)])
