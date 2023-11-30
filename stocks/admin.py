e4150d41ee25ab2cd51a382de0bbe1ccf995122d
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-30 15:57:00 +0300

    Add models for admin panel

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from stocks.models import Crypto, Order, Subscription, Wallet
+
+
+@admin.register(Crypto)
+class CryptoAdmin(admin.ModelAdmin):
+    list_display = ("name", "capacity", "exchange_rate")
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ("user", "crypto", "amount")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("user", "crypto", "order_type", "total_price", "amount", "is_auto")
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "crypto")
