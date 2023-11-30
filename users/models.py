ef90b8cdec805d2545f22f849c74ae92a554d4d1
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-11-30 16:22:36 +0300

    Update logic for logout

--- users/models.py
+++ users/models.py
@@ -1,4 +1,9 @@
+from datetime import datetime, timedelta
+
+import jwt
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core.cache import cache
 from django.db import models
 
 from users.managers import UserManager
@@ -25,3 +30,30 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+    @property
+    def token(self):
+        return self._generate_jwt_token()
+
+    def _generate_jwt_token(self):
+        dt = datetime.now() + timedelta(days=1)
+
+        token = jwt.encode(
+            {"id": self.pk, "exp": int(dt.strftime("%s"))},
+            settings.SECRET_KEY,
+            algorithm="HS256",
+        )
+        return token.decode("utf-8")
+
+    def logout(self, token):
+        current_time = datetime.now().strftime("%s")
+        cache.set(
+            token, "blacklisted", timeout=int(self._get_exp(token)) - int(current_time)
+        )
+
+    def _get_exp(self, token):
+        decoded = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        return decoded.get("exp")
+
+    def is_token_blacklisted(self, token):
+        return cache.get(token) == "blacklisted"
