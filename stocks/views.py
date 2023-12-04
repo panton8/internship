c72f76c48f029f308a968e6b3b9b14b04d546b51
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-12-04 13:09:46 +0300

    Add views for stocks app

--- stocks/views.py
+++ stocks/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from stocks.models import Crypto, Order, Subscription, Wallet
 from stocks.serializers import (CryptoSerializer, OrderSerializer,
                                 SubscriptionSerializer, WalletSerializer)
+from users.models import User
 
 
 class WalletViewSet(
@@ -17,19 +18,41 @@ class WalletViewSet(
 
 
 class OrderViewSet(
-    mixins.ListModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
 ):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        if self.action == "list":
+            if self.request.user.role not in [User.Roles.ADMIN, User.Roles.ANALYST]:
+                return Order.objects.filter(user=self.request.user)
+        return self.queryset
 
-class CryptoViewSet(
-    mixins.ListModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet
-):
+
+class CryptoViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Crypto.objects.all()
     serializer_class = CryptoSerializer
     permission_classes = [AllowAny]
+
+    @action(
+        detail=False,
+        methods=["post"],
+        permission_classes=[IsAuthenticated],
+        serializer_class=CryptoSerializer,
+    )
+    def add(self, request):
+        if request.user.is_superuser:
+            new_crypto = request.data
+            serializer = self.serializer_class(data=new_crypto)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_403_FORBIDDEN)
 
 
 class SubscriptionViewSet(
@@ -38,3 +61,12 @@ class SubscriptionViewSet(
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
     permission_classes = [IsAuthenticated]
+
+    @action(
+        detail=False,
+        methods=["post"],
+        permission_classes=[IsAuthenticated],
+        serializer_class=...,
+    )
+    def add(self, request):
+        pass
