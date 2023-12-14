743a62b0073d22853a3889c076a83577bc4d1e15
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-12-15 01:37:22 +0300

    Add statement for auto order

--- stocks/signals.py
+++ stocks/signals.py
@@ -1,11 +1,13 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Order
-from .tasks import complete_order
+from .models import Crypto, Order
+from .tasks import complete_auto_order, complete_order
 
 
 @receiver(post_save, sender=Order)
 def close_order(sender, instance, created, **kwargs):
     if created and not instance.is_auto:
-        complete_order(instance)
+        complete_order.delay(instance.pk)
+    if created and instance.is_auto:
+        complete_auto_order.delay(instance.pk)
