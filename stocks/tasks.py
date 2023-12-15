0f377204d72c662e17b8562c441f40ba00a6b408
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-12-15 22:25:40 +0300

    Add app task for common logic

--- stocks/tasks.py
+++ stocks/tasks.py
@@ -1,9 +1,38 @@
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
+from crypto_project.celery import app
 from stocks.models import Crypto, History, Order, Wallet
+from stocks.utils import order_possible_complete_check
 
 logger = get_task_logger(__name__)
+
+
+@app.task
+def close_order(order, user, possible_wallet):
+    if order_possible_complete_check(user, order, possible_wallet):
+        if order.order_type == Order.OrderType.PURCHASE:
+            user.balance -= order.total_price
+            user.save()
+            if possible_wallet:
+                possible_wallet.amount += order.amount
+                possible_wallet.save()
+            else:
+                Wallet.objects.create(
+                    user=order.user, crypto=order.crypto, amount=order.amount
+                )
+        else:
+            user.balance += order.total_price
+            user.save()
+            possible_wallet.amount -= order.amount
+            possible_wallet.save()
+        History.objects.create(
+            username=order.user.username,
+            crypto_name=order.crypto.name,
+            total_price=order.total_price,
+            amount=order.amount,
+        )
+        Order.objects.get(pk=order.pk).delete()
 
 
 @shared_task
@@ -11,28 +40,7 @@ def complete_order(order_pk):
     order = Order.objects.get(pk=order_pk)
     user = order.user
     possible_wallet = Wallet.objects.filter(crypto=order.crypto).first()
-    if order.order_type == Order.OrderType.PURCHASE:
-        user.balance -= order.total_price
-        user.save()
-        if possible_wallet:
-            possible_wallet.amount += order.amount
-            possible_wallet.save()
-        else:
-            Wallet.objects.create(
-                user=order.user, crypto=order.crypto, amount=order.amount
-            )
-    else:
-        user.balance += order.total_price
-        user.save()
-        possible_wallet.amount -= order.amount
-        possible_wallet.save()
-    History.objects.create(
-        username=order.user.username,
-        crypto_name=order.crypto.name,
-        total_price=order.total_price,
-        amount=order.amount,
-    )
-    Order.objects.get(pk=order.pk).delete()
+    close_order(order, user, possible_wallet)
 
 
 @shared_task
@@ -42,30 +50,9 @@ def complete_auto_order(crypto_pk, new_ex_rate):
     crypto.save()
     orders = Order.objects.all()
     for order in orders:
+        order.fill_amount_or_price()
+        order.save()
         if order.crypto.exchange_rate <= order.desired_exchange_rate:
             user = order.user
             possible_wallet = Wallet.objects.filter(crypto=order.crypto).first()
-            order.fill_amount_or_price()
-            order.save()
-            if order.order_type == Order.OrderType.PURCHASE:
-                user.balance -= order.total_price
-                user.save()
-                if possible_wallet:
-                    possible_wallet.amount += order.amount
-                    possible_wallet.save()
-                else:
-                    Wallet.objects.create(
-                        user=order.user, crypto=order.crypto, amount=order.amount
-                    )
-            else:
-                user.balance += order.total_price
-                user.save()
-                possible_wallet.amount -= order.amount
-                possible_wallet.save()
-            History.objects.create(
-                username=order.user.username,
-                crypto_name=order.crypto.name,
-                total_price=order.total_price,
-                amount=order.amount,
-            )
-            Order.objects.get(pk=order.pk).delete()
+            close_order(order, user, possible_wallet)
