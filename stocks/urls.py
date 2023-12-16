baa10fdf48eae1ae82737541ea8b5bbcf1248ec6
Anton Padvalnikau <podvalnikov0@gmail.com>
2023-12-17 00:33:53 +0300

    Add history url

--- stocks/urls.py
+++ stocks/urls.py
@@ -1,14 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from stocks.views import (CryptoViewSet, OrderViewSet, SubscriptionViewSet,
-                          WalletViewSet)
+from stocks.views import (CryptoViewSet, HistoryViewSet, OrderViewSet,
+                          SubscriptionViewSet, WalletViewSet)
 
 router = DefaultRouter()
 router.register("wallet", WalletViewSet)
 router.register("orders", OrderViewSet)
 router.register("cryptos", CryptoViewSet)
 router.register("subs", SubscriptionViewSet)
+router.register("history", HistoryViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
